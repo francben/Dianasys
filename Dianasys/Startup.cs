9ae1fcf2db19fd15e72b28ea8ceb25c19ca687b0
Franc <franczore@gmail.com>
2016-08-22 22:19:42 -0400

    Plantilla de proyecto Dianasys

---
+++ Dianasys/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dianasys.Startup))]
+namespace Dianasys
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
